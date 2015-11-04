36aab4aed93e77f2e240e944b4b8f2a190bd4af9
Neil Sharpe <nsharpe@gmail.com>
2015-11-03 17:45:33 -0800

    initial commit

---
+++ src/test/java/org/neil/imagej/ImageJInstallTest.java
@@ -0,0 +1,13 @@
+package org.neil.imagej;
+
+import org.junit.Test;
+
+/**
+ * Created by neilsharpe on 11/3/15.
+ */
+public class ImageJInstallTest {
+    @Test
+    public void testInstall(){
+
+    }
+}
