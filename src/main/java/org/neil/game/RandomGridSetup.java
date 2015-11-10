2d5c225454450b87515ceaf1f6394b946df97370
Neil Sharpe <nsharpe@gmail.com>
2015-11-09 17:21:47 -0800

    Add service to create a random grid of positions

--- src/main/java/org/neil/game/RandomGridSetup.java
+++ src/main/java/org/neil/game/RandomGridSetup.java
@@ -1,23 +1,43 @@
 package org.neil.game;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * Created by neilsharpe on 11/9/15.
  */
 public class RandomGridSetup {
-  private RandomGridSetup(){}
-
-
-
-  public static Stream<Position> makeMap(Double oddsOfExisting, Integer... axises){
-    Stream.of(axises)
+  private RandomGridSetup() {
   }
 
-  private static Boolean isAlive(Double ratio){
+  public static Stream<Position> makeMap(Double oddsOfExisting, Integer... axises) {
+    if (oddsOfExisting.compareTo(Double.valueOf("1")) > 0) {
+      throw new IllegalStateException("Odds cannot exceed 1");
+    }
+    if (oddsOfExisting.compareTo(Double.valueOf("0")) < 0) {
+      throw new IllegalStateException("Odds cannot be less then 0");
+    }
+    return mapPositions(new ArrayList<>(), axises).filter(x -> isAlive(oddsOfExisting));
+  }
+
+  public static Stream<Position> mapPositions(List<Integer> currentSetup, Integer... axises) {
+    if (currentSetup.size() == axises.length) {
+      return Stream.of(Position.of(currentSetup));
+    }
+    return IntStream.range(0, axises[currentSetup.size()])
+            .mapToObj(x -> x)//This is necessary to make flatMap work.  review at a later date.
+            .flatMap(x -> {
+              List<Integer> copy = new ArrayList<>(currentSetup);
+              copy.add(x);
+              return mapPositions(copy, axises);
+            });
+  }
+
+  private static Boolean isAlive(Double ratio) {
     return Math.random() <= ratio ? Boolean.TRUE : Boolean.FALSE;
   }
 }
--- src/test/java/org/neil/game/RandomGridSetupTest.java
+++ src/test/java/org/neil/game/RandomGridSetupTest.java
@@ -1,7 +1,41 @@
 package org.neil.game;
 
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by neilsharpe on 11/9/15.
  */
 public class RandomGridSetupTest {
+
+  @Test
+  public void testChanceToLiveToZero() {
+    Set<Position> result = RandomGridSetup.makeMap(Double.valueOf("0"), 3, 3)
+            .collect(Collectors.toSet());
+    assertTrue(result.isEmpty());
+  }
+
+  @Test
+  public void testGridSetup() {
+    Set<Position> result = RandomGridSetup.makeMap(Double.valueOf("1"), 3, 3)
+            .collect(Collectors.toSet());
+    assertEquals(9, result.size());
+    assertTrue(result.contains(Position.of(0, 0)));
+    assertTrue(result.contains(Position.of(0, 1)));
+    assertTrue(result.contains(Position.of(0, 2)));
+
+    assertTrue(result.contains(Position.of(1, 0)));
+    assertTrue(result.contains(Position.of(1, 1)));
+    assertTrue(result.contains(Position.of(1, 2)));
+
+    assertTrue(result.contains(Position.of(2, 0)));
+    assertTrue(result.contains(Position.of(2, 1)));
+    assertTrue(result.contains(Position.of(2, 2)));
+  }
 }
