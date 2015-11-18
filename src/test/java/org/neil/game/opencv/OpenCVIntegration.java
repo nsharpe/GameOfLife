8540d0a8a50f623b91296c3b5c017d054c625f4b
Neil Sharpe <nsharpe@gmail.com>
2015-11-17 23:21:50 -0800

    Integrate with opencv

---
+++ src/test/java/org/neil/game/opencv/OpenCVIntegration.java
@@ -0,0 +1,13 @@
+package org.neil.game.opencv;
+
+import org.junit.Test;
+
+/**
+ * Created by neilsharpe on 11/17/15.
+ */
+public class OpenCVIntegration {
+  @Test
+  public void testOpenCVCoreIntegration(){
+
+  }
+}
